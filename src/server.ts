7860bbfc21d95f6155710f3a04998e68e4fdb030
Lais Godinho <lais2bg@gmail.com>
2021-09-18 20:33:15 -0300

    solid

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express from "express";
+
+import { categoriesRoutes } from './routes/categories.routes';;
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json('Hello, world!');
-});
+app.use(express.json());
 
-app.listen(3333);
+app.use("/categories", categoriesRoutes);
+
+app.listen(3333, () => console.log("Server running on port 3333"));
